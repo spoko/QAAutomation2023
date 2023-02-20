783080b3552d7a96db10eb3549431b03a322da19
Ibraim Asanov <iasanov@nemetschek.bg>
2023-02-20 19:09:16 +0200

    Webhook test

--- src/test/java/base/TestUtil.java
+++ src/test/java/base/TestUtil.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import java.io.FileInputStream;
 import java.io.IOException;
